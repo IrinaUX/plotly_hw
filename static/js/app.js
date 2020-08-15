5289354a05f7f75176c213b652ec42ea1af2ad3a
Irina Kim <irinahpda@gmail.com>
2020-08-15 23:49:51 +0100

    initial commit.

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+
+console.log("Hello")
+
+var trace = {
+
+}
+
+var data = [trace];
+// Use `layout` to define a title
+var layout = {
+    title: "test subject ID 940"
+};
+
+// Render the plot to the `plot1` div
+Plotly.newPlot("plot", trace, layout);
+
