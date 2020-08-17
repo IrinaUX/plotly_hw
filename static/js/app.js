bc198beef599947dc221455733a7d4bb6c180641
Irina Kim <irinahpda@gmail.com>
2020-08-17 03:14:56 +0100

    Add plot for test ID 940 - no labels.

--- static/js/app.js
+++ static/js/app.js
@@ -1,62 +1,73 @@
-
 console.log("Hello");
 
-// var jsonSample = samples;
-// console.log(campl);
+// d3.json("samples.json").then(data => {
+// //   console.log(data);
+//   const sample_values = data.samples[0].sample_values;
+//   const otu_id = data.samples[0].otu_ids;
+//   const otu_labels = data.samples[0].otu_labels; //.slice(0, 10);
+//   console.log(`sample_values typeof - ${typeof(sample_values)}: ${sample_values.slice(0, 10)}`);
+//   console.log(`otu_id typeof - ${typeof(otu_id)}: ${otu_id.slice(0, 10)}`);
+//   console.log(`otu_labels typeof - ${typeof(otu_labels)}: ${otu_labels.slice(0, 10)}`);
+    // const trace = {
+    //   x: sample_values, //data.samples[0].sample_values, //.map(val => Math.sqrt(val)),
+    //   y: otu_ids,
+    //   type: 'bar',
+    //   text: otu_labels,
+    //   orientation: 'h',
+    //   name: "Bacteria",
+    // };
+// })
 
-// var trace = {
-//     x: samples.samples.sample_values,
-//     y: samples.names,
+// var data = [trace];
+// // Use `layout` to define a title
+// var layout = {
+//     title: "Test subject ID 940"
+// };
 
-// }
+// // Render the plot to the `plot1` div
+// Plotly.newPlot("plot", data, layout);
+// })
 
-function unpack(rows, index) {
-  return rows.map(function(row) {
-    return row[index];
-  });
-}
-let sampleValues = '', names = '';
-d3.json("samples.json").then((jsonData) => {
-  var data = jsonData;
-  function filterId940(id){
-    const jsonId = data.samples[0].id;
-    console.log(`jsonID - 0: ${jsonId}`);
-    return id === 940;
-  };
-  filterId940(940);
-  var id940Data = samples.filter(filterId940);
-  console.log(`id 940 data - 1: ${id940Data}`);
-  // console.log(`Typeof 0 - ${data}`);
-    //  Create the Traces
-    // console.log(data.samples[0].id);
-    // console.log(data.samples[0].otu_ids); // 
-    // console.log(data.samples[0].sample_values);
-    // console.log(data.samples[0].sample_values);
-    const name1 = unpack(data.samples[0].otu_labels, 0)[-1];
-    // const name2 = unpack(data.samples[0].otu_labels, 1)[-1];
-    sampleValues = data.samples[0].sample_values;
-    // ids = data.samples[0].otu_ids.slice(0, 10);
-    // names = data.samples[0].otu_labels[-1];//.slice(0, 10); // .slice(0, 10);
-    console.log(`Typeof - 1: ${name1}`);
-    // console.log(`Inside the json.then ${names1}`);
-    const trace = {
-      x: sampleValues, //data.samples[0].sample_values, //.map(val => Math.sqrt(val)),
-      y: name1,
-      type: "bar",
-      orientation: 'h',
-      name: "Cancer Survival",
-    };
+function init() {
+  // Grab a reference to the dropdown select element
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then(data => {
+      var sample940 = data.samples[0];
+      console.log(sample940);
+      const title = `Sample id - ${sample940.id}`;
+      const sample_values = sample940.sample_values;
+      const otu_ids = `OTU IDS: ${sample940.otu_ids}`;
+      const otu_labels = `OTU labels: ${sample940.otu_labels}`;
+        
+      console.log(title);
+      console.log(otu_ids);
+      console.log(otu_labels);
 
-console.log(`Names: ${names}`);
-// var samples = samples;
-// console.log(samples.samples.sample_values)
+      const trace = {
+        x: sample_values.slice(0, 10), //data.samples[0].sample_values, //.map(val => Math.sqrt(val)),
+        y: otu_ids.slice(0, 10),
+        type: 'bar',
+        orientation: 'h',
+        // text: otu_labels.slice(0, 10),
+        name: "Bacteria",
+        title: title,
+      };
+      var data = [trace];
+    // Use `layout` to define a title
+      var layout = {
+        title: "Test subject ID 940 - Top 10 Bacteria"
+      };
 
-var data = [trace];
-// Use `layout` to define a title
-var layout = {
-    title: "Test subject ID 940"
+      // Render the plot to the `plot1` div
+      Plotly.newPlot("plot", data, layout);
+      })
+      
+    // Use the first sample from the list to build the initial plots
+    // const firstSample = sampleNames[0];
+    // buildCharts(firstSample);
+    // buildMetadata(firstSample);
+
 };
 
-// Render the plot to the `plot1` div
-Plotly.newPlot("plot", data, layout);
-})
+
+init();
