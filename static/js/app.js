34caf543f1b23cd47797de02d4d83c8079d2bedc
Irina Kim <irinahpda@gmail.com>
2020-08-20 04:19:27 +0100

    Remove commented lines.

--- static/js/app.js
+++ static/js/app.js
@@ -37,52 +37,30 @@ function buildGraph(sample) {
     const names = data.names;
     const samples = data.samples;
     const sample_data = samples.filter(item => item.id == sample);
-    // console.log(data);
-    // console.log(sample_data);
     const sample_id = sample_data[0].id;
     const sample_values = sample_data[0].sample_values;
     const sample_otu_ids = sample_data[0].otu_ids;
     const sample_otu_labels = sample_data[0].otu_labels;
-    // console.log(`Sample id type: ${typeof(sample_id)}:`);
-    // console.log(sample_id);
-    // console.log(`Sample values type: ${typeof(sample_values)}:`);
-    // console.log(sample_values);
-    // console.log(`Sample otu ids type: ${typeof(sample_otu_ids)}:`);
-    // console.log(sample_otu_ids);
-    // console.log(`Sample otu labels: ${typeof(sample_otu_labels)}:`);
     console.log(sample_otu_labels)
     console.log(sample_otu_labels.length);
     const newObjList = [];
     for (var i=0; i < sample_otu_labels.length; i++) {
-      // console.log(sample_otu_labels[i]);
-      // console.log(typeof(sample_otu_labels[i]));
-      // console.log("------");
       const string_label = sample_otu_labels[i];
-      // console.log(string_label);
-      // console.log("------");
       const split_label_string = string_label.split(";");
-      // console.log(split_label_string);
-      // console.log(typeof(split_label_string));
-      // console.log(split_label_string.slice(0));
       const sliced_string = split_label_string.slice(-1);
       console.log(typeof(sliced_string));
       console.log(sliced_string);
       console.log("------");
       newObjList.push(sliced_string);
-
     }
-    console.log("~~~~~~~~~~~~~~~~~~");
-    console.log(typeof(newObjList));
-    console.log(newObjList.length);
-    console.log(newObjList);
-
+    
     var mergedLabels = [].concat.apply([], newObjList);
     console.log(mergedLabels);
     
     const title = `Sample id - ${sample_id}`;
     const trace = {
       x: sample_values.slice(0, 10), //data.samples[0].sample_values, //.map(val => Math.sqrt(val)),
-      y: sample_otu_ids.slice(0, 10),
+      y: mergedLabels.slice(0, 10),
       type: 'bar',
       orientation: 'h',
       title: title,
